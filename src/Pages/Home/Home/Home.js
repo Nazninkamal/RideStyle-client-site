42393f5e8ba71ec24ff5d7f88345db6fb6eb2391
NazninKamal <najninkamalrimi@gmail.com>
2021-11-09 22:48:06 +0600

    1st commit

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Services from '../Services/Services';
+import Navigation from '../Shared/Navigation/Navigation';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <Banner></Banner>
+            <Services></Services>
+            
+            
+            
+            
+
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Login/Firebase/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAWGyGUsfK0Jhp9aTr2duO4F-dgfENixDg",
+    authDomain: "assignment12-8e138.firebaseapp.com",
+    projectId: "assignment12-8e138",
+    storageBucket: "assignment12-8e138.appspot.com",
+    messagingSenderId: "872971634364",
+    appId: "1:872971634364:web:09f65be6be41114b06b2b9"
+  };
+
+  export default firebaseConfig;
